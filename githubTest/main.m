efaa15c28896ab974b735d0e54ee8053e2a60b04
jzhou <jzhou@ilegendsoft.com>
2014-10-15 17:02:50 +0800

    update

---
+++ githubTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  githubTest
+//
+//  Created by zhoujiuhai on 14/10/15.
+//  Copyright (c) 2014年 com.joechow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
